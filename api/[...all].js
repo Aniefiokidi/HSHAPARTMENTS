76eb3440a0d2eb819f87b3b62ee803aac6a14f22
Arnold Idiong <idiong.arnold@stu.cu.edu.ng>
2026-05-03 14:19:09 +0100

    Handle serverless startup errors without crashing function

--- api/[...all].js
+++ api/[...all].js
@@ -1,16 +1,18 @@
 require('dotenv').config({ path: './server/.env' });
 
-const app = require('../server/app');
-const connectDB = require('../server/config/db');
-
 module.exports = async (req, res) => {
   try {
+    const app = require('../server/app');
+    const connectDB = require('../server/config/db');
+
     await connectDB();
     return app(req, res);
   } catch (error) {
+    console.error('Serverless boot error:', error);
     return res.status(500).json({
       success: false,
-      message: 'Database connection failed',
+      message: 'Serverless function startup failed',
+      error: error.message,
     });
   }
 };
